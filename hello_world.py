6043526ef28a0d7e39ec4b36b6172a9e01ad66ec
binh nguyen <thanhbinhhebi@gmail.com>
2025-03-15 13:40:53 +0700

    <test>: New commit

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+a, b, c =map(int, input().split())
+
+print("The sum = ", a+b+c)
+
+max = a if (a>b) and (a> c) else b if (b>c) else c
+
+print("The max number: ", max)
+print("What's next?")
+print("Creating new branch.")
+
+print(f'The sum {a+b+c}')
+print("The end")
+
+print("What's the next step?")
+
